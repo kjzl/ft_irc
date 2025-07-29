691206f79bf186f08013b923f0f62701db25be3e
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-07-29 17:15:21 +0200

    got somewhere

--- include/Server.hpp
+++ include/Server.hpp
@@ -1,8 +1,12 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
+#include <map>
 #include <vector>
 #include <string>
+
+#define	BACKLOG 10
+#define	TIMEOUT	2500 // = 2.5 seconds waiting for events
 
 class	Client;
 class	Channel;
@@ -15,17 +19,27 @@ class Server {
 
 		Server(const Server& other);
 		Server& operator=( const Server& other );
+		void	waitForRequests(void);
 
-		// Getters and setters
-		int	serverInit(void);
-		int	serverSocketCreate(void);
 	private:
-		const int					port_;
-		int							serverSocket_;
-		static bool					signal_;
-		std::vector<struct pollfd>	pollFds_;
-		std::vector<Client>			clients_;
-		std::vector<Channel>		channels_;
+		// Getters and setters
+		int			getPort( void ) const;
+		int			getServerSocket( void ) const;
+		void		setServerSocket( int serverSocketFd );
+		void	addPollFd(const int fd, const short events, const short revents);
+
+		void		createListeningSocket(void);
+		void		serverInit(void);
+		void	serverShutdown(void);
+		void	acceptConnection();
+		static void	signalHandler(int signum);
+
+		const int						port_;
+		int								serverSocket_;
+		static bool						running;
+		std::vector<struct pollfd>		pollFds_;
+		std::map<std::string, Client>	clients_;
+		std::map<std::string, Channel>	channels_;
 };
 
 #endif // !SERVER_HPP
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,6 +1,27 @@
 #include "../include/Server.hpp"
 #include "../include/Debug.hpp"
+#include <csignal>
+#include <iostream>
 #include <netinet/in.h>
+#include <stdexcept>
+#include <sys/socket.h>
+#include <errno.h>
+#include <netdb.h>
+#include <poll.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/poll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+//TODO: addPollFd clients: allways POLLIN | POLLHUP
+
+void	Server::signalHandler(int signum)
+{
+	static_cast<void>(signum);
+	running = false;
+}
 
 // Default Constructor
 Server::Server(void): port_(6667)
@@ -12,7 +33,8 @@ Server::Server(void): port_(6667)
 Server::Server(int port, std::string password): port_(port), serverSocket_(-1)
 {
 	debug("Parameterized Constructor called");
-	pollFds_.reserve(5);
+	signal(SIGINT, Server::signalHandler);
+	signal(SIGQUIT, Server::signalHandler);
 }
 
 // Destructor
@@ -37,17 +59,111 @@ Server& Server::operator=(const Server& other)
 	return *this;
 }
 
-int	Server::serverSocketCreate(void)
+int	Server::getPort( void ) const
 {
-	struct	sockadr_in sa;
-
-	sa.sin_family
+	return (port_);
 }
 
-int	Server::serverInit(void)
+int	Server::getServerSocket( void ) const
 {
-	serverSocket_ = serverSocketCreate();
+	return this->serverSocket_;
+}
+
+void	Server::addPollFd(const int fd, const short events, const short revents)
+{
+	struct pollfd newPollfd = {fd, events, revents};
+	pollFds_.push_back(newPollfd);
+}
+
+void	Server::setServerSocket( int serverSocketFd )
+{
+	this->serverSocket_ = serverSocketFd;
+}
+
+void	Server::acceptConnection(int pollIndex)
+{
+	int clientFd;
+
+	clientFd = accept(getServerSocket(), NULL, NULL);
+	if (clientFd == -1)
+		throw std::runtime_error("[Server] accept error");
+	addPollFd(clientFd, POLLIN | POLLHUP, 0);
+	debug("server accepted new connection");
+
+}
+
+// this function should check if its a POLLHUP or POLLIN 
+// then interpret the message
+// and execute it
+void	Server::readFromSocket(struct pollfd request)
+{
+	//TODO:
+}
+
+void	Server::waitForRequests(void)
+{
+	while (running)
+	{
+		int rdyPollsCount = poll(&(pollFds_[0]), pollFds_.size(), TIMEOUT);
+		if (rdyPollsCount == -1)
+			throw std::runtime_error("[Server] poll error");
+		else if (rdyPollsCount == 0)
+		{
+			std::cout << "[Server] Waiting for requests" << std::endl;
+			continue;
+		}
+		int	rdyPollsChecked;
+		for (int pollIndex = 0; pollIndex < pollFds_.size() && rdyPollsChecked < rdyPollsCount; pollIndex++)
+		{ 
+			if (((pollFds_[pollIndex].revents & (POLLIN | POLLHUP)) != 1))
+				continue; // this socket is not the ready one
+			++rdyPollsChecked;
+			if (pollIndex == 0)
+				acceptConnection();
+			else
+				readFromSocket(pollFds_[pollIndex]);
+		}
+	}
+	std::cout << YEL << "[Server] Stopped listening for requests" << RESET << std::endl;
+}
+
+void	Server::createListeningSocket(void)
+{
+	struct	sockaddr_in	sa = {0};
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(getPort());
+	sa.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	//create Socket
+	serverSocket_ = socket(sa.sin_family, SOCK_STREAM, 0);
 	if (serverSocket_ == -1)
-		return (-1);
+		throw std::runtime_error("[Server] socket error");
+	//bind socket
+	if (-1 == bind(getServerSocket(), reinterpret_cast<struct sockaddr *>(&sa), sizeof sa))
+		throw std::runtime_error("[Server] bind error");
+	// mark as passive socket listening to incoming connections from clients
+	if (-1 == listen(getServerSocket(), BACKLOG))
+		throw std::runtime_error("[Server] listen error");
 }
 
+void	Server::serverInit(void)
+{
+	pollFds_.reserve(5);
+
+	this->createListeningSocket();
+	this->addPollFd(getServerSocket(), POLLIN, 0);
+}
+
+void	Server::serverShutdown(void)
+{
+	std::cout << BRED << "==== STARTING SERVER SHUTDOWN ====" << RESET << std::endl;
+	//TODO:
+	// loop through clients and close their fd
+	if (serverSocket_ != -1)
+	{
+		if (-1 == close(serverSocket_))
+			throw std::runtime_error(strerror(errno));
+		std::cout << RED << "[Server] diconnected listening socket" << RESET << std::endl;
+	}
+	std::cout << GREEN << "[Server] Shutdown complete" << RESET << std::endl;
+}
