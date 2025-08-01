a9577ee0d4a8c5948c87906fe1816fa003756ebd
kjzl <kjzl@posteo.de>
2025-08-01 14:34:01 +0200

    Use CaseMappedString instead of std::string for storing the nickname

--- include/Client.hpp
+++ include/Client.hpp
@@ -3,16 +3,17 @@
 
 #include <cstdio>
 #include <string>
+#include "CaseMappedString.hpp"
 
 class   Client
 {
 	private:
-		bool        is_authenticated_;
-		int			socket_;
-		std::string nickname_;
-		std::string username_;
-		std::string realname_;
-		std::string	rawMessage_;
+		bool				is_authenticated_;
+		int					socket_;
+		CaseMappedString	nickname_;
+		std::string			username_;
+		std::string			realname_;
+		std::string			rawMessage_;
 
 
 	public:
@@ -21,19 +22,19 @@ class   Client
 		Client &operator =(const Client &other);
 		virtual ~Client();
 
-		bool isAuthenticated() const;
-		const std::string &getNickname() const;
-		const std::string &getUsername() const;
-		const std::string &getRealname() const;
-		const std::string &getRawMessage() const;
-		int getSocket() const;
+		bool isAuthenticated()	const;
+		const CaseMappedString	&getNickname() const;
+		const std::string		&getUsername() const;
+		const std::string		&getRealname() const;
+		const std::string		&getRawMessage() const;
+		int getSocket()			const;
 
 		void setAuthenticated(bool authenticated);
 		void setNickname(const std::string &nickname);
 		void setUsername(const std::string &username);
 		void setRealname(const std::string &realname);
 		void setRawMessage(const std::string &rawMessage);
-		void setSocket(int socket); 
+		void setSocket(int socket);
 
 		void appendRawMessage(const char partialMessage[BUFSIZ], size_t length);
 		void clearMessage();
