a57457907f55f583403f7afc0656ed5526b0ffe0
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-08-05 13:04:17 +0200

    removed duplicate declaration

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -24,12 +24,6 @@ class   Channel
 		Channel(const Channel &other);
 		Channel &operator =(const Channel &other);
 		virtual ~Channel();
-		const std::vector<std::string> &getMembers() const;
-		const std::set<std::string> &getWhiteList() const;
-		const std::set<std::string> &getOperators() const;
-		const std::string &getTopic() const;
-		const std::string &getPassword() const;
-		int getUserLimit() const;
 
 		// Getters => necessary or only Utils?? 
 		const std::vector<std::string> &getMembers() const;
