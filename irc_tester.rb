703a831298352cae06b3e609b5b9a6b82b4058d5
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-08-27 17:20:42 +0200

    starting the write off

--- irc_tester.rb
+++ irc_tester.rb
@@ -1,7 +1,22 @@
 #!/usr/bin/env ruby
 require 'open3'
 
-system("make") or raise "Failed to compile ircserv!"
+class IrcservTester
+  attr_reader :s_stdout, :s_stderr
+  def initialize(port: 6667, password: "password")
+    @port = port
+    @password = password
+    @s_stdout = ""
+    @s_stderr = ""
+    @n_stdout = ""
+  end
+  def start_server
+    puts "Starting IRC server on port #{@port} with password '#{@password}'..."
+    system("make") or raise "Failed to compile ircserv!"
+    @s_thread = Thread.new do
+      Open3.popen3("./ircserv #{@port} #{@password}") do |stdin, stdout, stderr, wait_thread|
+        @server_pid = wait_thread.pid
+    end
 timeout = 5
 
 puts "Starting server for #{timeout} seconds"
